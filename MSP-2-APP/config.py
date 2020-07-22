c3fe63c390b896085be2d10af1f622ddf213dd74
flabastie <flabastie@hotmail.com>
2020-07-22 16:19:35 +0200

    replaced config

---
+++ MSP-2-APP/config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+api_key = 'db8e4602d7df4f6aa85d537558c3a63a'
+api_url = 'https://francecentral.api.cognitive.microsoft.com/contentmoderator/moderate/v1.0/ProcessText/Screen?classify=True'
+db_path = '/home/francois/workspace/10_formation/MSP-2-APP/comments.db'
